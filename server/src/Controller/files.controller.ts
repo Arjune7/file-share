20ce520e088d9e978710ecbce87baad03322d04d
Arjune7 <arjungrover947@gmail.com>
2023-02-27 12:11:55 +0530

    changes

--- server/src/Controller/files.controller.ts
+++ server/src/Controller/files.controller.ts
@@ -3,8 +3,10 @@ import multer from "multer";
 import { UploadApiResponse, v2 as cloudinary } from "cloudinary";
 import File from "../Models/File";
 import dotenv from "dotenv";
+import nodemailer from "nodemailer";
 
-import https from 'https'
+import https from "https";
+import createEmailTemplate from "../utils/emailTemp";
 
 dotenv.config();
 const storage = multer.diskStorage({});
@@ -57,16 +59,15 @@ export const getFileById = async (req: express.Request, res: express.Response) =
       return res.status(404).json({ message: "file doesnt exist" });
     }
 
-    const {fileName , format , sizeInBytes} = file
+    const { fileName, format, sizeInBytes } = file;
     return res.status(200).json({
-      name : fileName,
+      name: fileName,
       sizeInBytes,
       format,
-      id
-    })
+      id,
+    });
   } catch (error) {
     return res.status(500).json({ message: "server error" });
-
   }
 };
 
@@ -78,13 +79,57 @@ export const downloadFileById = async (req: express.Request, res: express.Respon
       return res.status(404).json({ message: "file doesnt exist" });
     }
 
-    if(file.secure_url){
+    if (file.secure_url) {
       const url = file.secure_url.toString();
-      https.get(url , (fileStream) => fileStream.pipe(res))
+      https.get(url, (fileStream) => fileStream.pipe(res));
     }
-
   } catch (error) {
     return res.status(500).json({ message: "server error" });
-
   }
+};
+
+export const sendEmail = async (req: express.Request, res: express.Response) => {
+  const { id, emailFrom, emailTo } = req.body;
+
+  const file = await File.findById(id);
+  if (!file) {
+    return res.status(404).json({ message: "file doesnt exist" });
+  }
+
+  let transporter = nodemailer.createTransport({
+    //@ts-ignore
+    host: process.env.SENDINBLUE_SMTP_HOST!,
+    port: process.env.SENDINBLUE_SMTP_PORT,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: process.env.SENDINBLUE_SMTP_USER, // generated ethereal user
+      pass: process.env.SENDINBLUE_SMTP_PASSWORD, // generated ethereal password
+    },
+  });
+
+  const { fileName, sizeInBytes } = file;
+
+  const fileSize = `${(Number(sizeInBytes) / (1024 * 1024)).toFixed(2)} MB`;
+  const downloadLink = `${process.env.API_BASE_ENDPOINT_CLIENT}download/${id}`;
+
+  const mailOptions = {
+    from: emailFrom, // sender address
+    to: emailTo, // list of receivers
+    subject: "File Shared with you ✔", // Subject line
+    text: `${emailFrom} sharre a file with you`, // plain text body
+    html: createEmailTemplate(emailFrom, emailTo, downloadLink, fileName, fileSize), // html body
+  };
+
+  transporter.sendMail(mailOptions, async (error, info) => {
+    if (error) {
+      console.log(error);
+      return res.status(500);
+    }
+
+    file.sender = emailFrom
+    file.reciever = emailTo
+
+    await file.save();
+    return res.status(200).json({message : 'Email Sent'})
+  });
 };
--- server/src/Routes/files.router.ts
+++ server/src/Routes/files.router.ts
@@ -1,10 +1,11 @@
 import express from 'express'
-import { downloadFileById, getFileById, upload, uploadFiles } from '../Controller/files.controller'
+import { downloadFileById, getFileById, sendEmail, upload, uploadFiles } from '../Controller/files.controller'
 
 const router = express.Router()
 
 router.post("/upload" , upload.single("myFile") , uploadFiles)
 router.get("/:id/download" , downloadFileById)
 router.get("/:id" , getFileById)
+router.post("/email" , sendEmail)
 
 export default router
